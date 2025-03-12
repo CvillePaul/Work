274d72b924ae3de023ceabf7a7d7f393f7dfe969
Cville Paul <90533299+CvillePaul@users.noreply.github.com>
2025-03-12 17:51:12 -0400

    feat: first commit

---
+++ 2025-03-10 LBT Observing 2025-03-14/to_pdf.py
@@ -0,0 +1,8 @@
+import glob
+
+import astropaul.html as html
+
+for categorical_file in glob.glob("../../Observing Files/LBT Observing 2025-03-14/Categorical Priorities *.html"):
+
+    html.html_to_pdf(categorical_file, categorical_file.replace(".html", ".pdf"))
+    print(categorical_file)
