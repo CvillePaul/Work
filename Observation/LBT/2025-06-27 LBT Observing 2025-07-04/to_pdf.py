890ac20167778c6a378264ef00383278a37ec359
Cville Paul <90533299+CvillePaul@users.noreply.github.com>
2025-06-29 17:24:58 -0400

    feat: add files for LBT observing 2025-07-04

---
+++ Observation/LBT/2025-06-27 LBT Observing 2025-07-04/to_pdf.py
@@ -0,0 +1,10 @@
+import glob
+import pathlib
+
+import astropaul.html as html
+
+for categorical_file in glob.glob("../../../../Observing Files/LBT Observing 2025-07-04/Categorical Priorities *.html"):
+    input_file = pathlib.Path(categorical_file)
+    output_file = input_file.name.replace(".html", ".pdf")
+    html.html_to_pdf(input_file, output_file)
+    print(output_file)
