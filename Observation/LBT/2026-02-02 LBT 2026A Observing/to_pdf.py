33f43ad465b6eca3b01e00c9641b07fa6190a2ea
Cville Paul <90533299+CvillePaul@users.noreply.github.com>
2026-04-30 17:46:32 -0400

    feat: minor

--- Observation/LBT/2026-02-02 LBT 2026A Observing/to_pdf.py
+++ Observation/LBT/2026-02-02 LBT 2026A Observing/to_pdf.py
@@ -3,7 +3,7 @@ import pathlib
 
 import astropaul.html as html
 
-for categorical_file in glob.glob("../../../../Observing Files/LBT Observing 2026-02-17/Categorical Priorities *.html"):
+for categorical_file in glob.glob("../../../../Observing Files/LBT Observing 2026-05-03/Categorical Priorities *.html"):
     input_file = pathlib.Path(categorical_file)
     output_file = input_file.name.replace(".html", ".pdf")
     html.html_to_pdf(input_file, output_file)
