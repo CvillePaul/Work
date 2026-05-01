2880982c876e5150c8e2306c327479f3c8014b84
Cville Paul <90533299+CvillePaul@users.noreply.github.com>
2026-05-01 11:49:35 -0400

    feat: handle new file naming scheme

--- Observation/LBT/2026-02-02 LBT 2026A Observing/to_pdf.py
+++ Observation/LBT/2026-02-02 LBT 2026A Observing/to_pdf.py
@@ -3,7 +3,7 @@ import pathlib
 
 import astropaul.html as html
 
-for categorical_file in glob.glob("../../../../Observing Files/LBT Observing 2026-05-03/Categorical Priorities *.html"):
+for categorical_file in glob.glob("../../../../Observing Files/LBT Observing 2026-05-03/uva_multistar*.html"):
     input_file = pathlib.Path(categorical_file)
     output_file = input_file.name.replace(".html", ".pdf")
     html.html_to_pdf(input_file, output_file)
